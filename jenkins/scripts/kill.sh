c759f04c82c229eaa66f0c999b3114c70fd92562
Daniel Tan Zhonghao <danieltan.tan199889@gmail.com>
2022-11-13 00:29:03 +0800

    qwe

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env sh
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
 
-set -x
-docker kill my-apache-php-app
-docker rm my-apache-php-app
+\f0\fs24 \cf0 #!/usr/bin/env sh\
+\
+set -x\
+docker kill my-apache-php-app\
+docker rm my-apache-php-app }
